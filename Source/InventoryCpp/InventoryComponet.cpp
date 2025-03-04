d09930d9c5963b55d7a744a8a324942608909c7d
AR <72244488+sundev-nalees@users.noreply.github.com>
2025-03-04 14:04:06 +0530

    Inventory Component
    ---Added Inventory Component with add and remove feature

--- Source/InventoryCpp/InventoryComponet.cpp
+++ Source/InventoryCpp/InventoryComponet.cpp
@@ -32,3 +32,95 @@ void UInventoryComponet::TickComponent(float DeltaTime, ELevelTick TickType, FAc
 	// ...
 }
 
+bool UInventoryComponet::AddItem(FInventoryItemData NewItem)
+{
+	float ItemTotalWeight = NewItem.Weight * NewItem.Quantity;
+
+	if(CurrentWeight+ItemTotalWeight>MaxWeight)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Cannot add %s. Over the Max Weight Limit!!!!!"), *NewItem.ItemName.ToString());
+		return false;
+	}
+	
+	Items.Add(NewItem);
+	CurrentWeight += ItemTotalWeight;
+
+	UE_LOG(LogTemp, Warning, TEXT("%s Added to the Inventory . Current Weight : %f/%f"), *NewItem.ItemName.ToString(),CurrentWeight,MaxWeight);
+
+	return true;
+}
+
+bool UInventoryComponet::RemoveItem(FName ItemName, int32 Quantity)
+{
+	for(int32 i=0;i<Items.Num();i++)
+	{
+		if (Items[i].ItemName == ItemName)
+		{
+			if (Items[i].Quantity < Quantity)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("Not enough %s to remove!"), *ItemName.ToString());
+				return false;
+			}
+
+			float WeightToRemove = Items[i].Weight * Quantity;
+			Items[i].Quantity -= Quantity;
+			CurrentWeight -= WeightToRemove;
+
+			if (Items[i].Quantity <= 0)
+			{
+				Items.RemoveAt(i);
+			}
+
+			UE_LOG(LogTemp, Warning, TEXT("%s Removed From the Inventory . Current Weight : %f/%f"), *ItemName.ToString(), CurrentWeight, MaxWeight);
+
+			return true;
+		}
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Item %s not Found!!!"), *ItemName.ToString());
+
+	return false;
+}
+
+EWeightStage UInventoryComponet::GetWeightStage() const
+{
+	if(CurrentWeight<MaxWeight*0.5)
+	{
+		return EWeightStage::NORMAL;
+	}
+	else if(CurrentWeight<MaxWeight*0.8)
+	{
+		return EWeightStage::HEAVY;
+	}
+	else
+	{
+		return EWeightStage::OVERBURDENED;
+	}
+	
+}
+
+void UInventoryComponet::DisplayInventory() const
+{
+	UE_LOG(LogTemp, Log, TEXT("------------------Inventory-----------------"));
+
+	for(const auto& Item:Items)
+	{
+		UE_LOG(LogTemp, Log, TEXT("- %s x%d (Weight: %f)"), *Item.ItemName.ToString(), Item.Quantity, Item.Weight * Item.Quantity);
+	}
+	UE_LOG(LogTemp, Log, TEXT("Total Weight: %f/%f"), CurrentWeight, MaxWeight);
+	EWeightStage Stage = GetWeightStage();
+	FString WeightStatus;
+	switch (Stage) {
+	case EWeightStage::NORMAL:
+		WeightStatus = "Normal";
+		break;
+	case EWeightStage::HEAVY:
+		WeightStatus = "Heavy - Movement slightly reduced";
+		break;
+	case EWeightStage::OVERBURDENED:
+		WeightStatus = "Overburdened - Cannot move";
+		break;
+	}
+	UE_LOG(LogTemp, Log, TEXT("Weight Status: %s"), *WeightStatus);
+}
+
--- Source/InventoryCpp/InventoryComponet.h
+++ Source/InventoryCpp/InventoryComponet.h
@@ -6,6 +6,34 @@
 #include "Components/ActorComponent.h"
 #include "InventoryComponet.generated.h"
 
+UENUM(BlueprintType)
+enum class EWeightStage:uint8
+{
+	NORMAL,
+	HEAVY,
+	OVERBURDENED
+};
+
+USTRUCT(BlueprintType)
+
+struct FInventoryItemData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	FName ItemName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	float Weight;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
+	int32 Quantity;
+
+	FInventoryItemData():ItemName("Unknown"),Weight(0.0f),Quantity(1){}
+
+	FInventoryItemData(FName Name,float ItemWeight,int32 ItemQuantity):ItemName(Name),Weight(ItemWeight),Quantity(ItemQuantity){}
+
+};
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class INVENTORYCPP_API UInventoryComponet : public UActorComponent
@@ -23,6 +51,29 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inventory")
+	float MaxWeight;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Inventory")
+	float CurrentWeight;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Inventory")
+	TArray<FInventoryItemData>Items;
+
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	bool AddItem(FInventoryItemData NewItem);
+
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	bool RemoveItem(FName ItemName, int32 Quantity);
+
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	EWeightStage GetWeightStage() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	void DisplayInventory() const;
+
 
 		
 };
